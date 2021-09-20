96df631d99566470cdba5620d642594717b4d704
Ritesh Agarwal <ju7stritesh@gmail.com>
2021-09-20 22:10:57 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -22,16 +22,7 @@ function App() {
 
 	<!-- <script src="video.js"></script> -->
 	<link href="https://unpkg.com/video.js/dist/video-js.min.css" rel="stylesheet">
-	<!-- <script src="node_modules/video.js/dist/video.js"></script> -->
-	<!-- <script src="//vjs.zencdn.net/7.10.2/video.min.js"></script> -->
-	<!-- <script src="dash.js-4.0.1/dist/dash.all.min.js"></script> -->
-	<script src="https://unpkg.com/video.js/dist/video.js"></script>
-	<!-- <script src="videojs-dash.min.js"></script> -->
-	<script src="https://unpkg.com/videojs‐contrib‐dash/dist/videojs‐dash.js"></script>
-	<script>
-	var player = videojs('example-video');
-	player.src({ src: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8', type: 'application/x-mpegURL'});
-	player.play();
+	
       </header>
     </div>
   );
