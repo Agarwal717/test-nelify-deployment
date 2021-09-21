d2662679b17a7a2022a242746e5aec3fe7059a00
Ritesh Agarwal <ju7stritesh@gmail.com>
2021-09-21 16:33:31 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -23,4 +23,16 @@ function App() {
   );
 }
 
+var foo = function(){
+  var button = document.createElement('button');
+  button.innerHTML = 'click me';
+  button.onclick = function(){
+    alert('here be dragons');return false;
+  };
+  // where do we want to have the button to appear?
+  // you can append it to another element just by doing something like
+  // document.getElementById('foobutton').appendChild(button);
+  document.body.appendChild(button);
+};
+
 export default App;
